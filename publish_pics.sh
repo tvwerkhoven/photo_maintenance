ef76d572e9a1ec89affe24cbd1e16cc2ba776bb2
Tim van Werkhoven <tvwerkhoven@github>
2021-10-31 20:31:49 +0100

    Overwrite tag in place, preserve file mod time

--- publish_pics.sh
+++ publish_pics.sh
@@ -589,7 +589,7 @@ _convert_pics() {
         # https://developers.google.com/speed/docs/insights/OptimizeImages
         ${_PROG_CONVERT} -geometry 1920x1920\> -quality 60 "${_SOURCE_DIR}/${_imgfile}" "${_EXPORT_DIR}/${_imgfileout}"
         # Delete Burst UUID tag (if set) because somehow it gets broken while converting causing iOS to not recognize the converted image
-        ${_PROG_EXIFTOOL} -makernotes:burstuuid= "${_EXPORT_DIR}/${_imgfileout}"
+        ${_PROG_EXIFTOOL} -makernotes:burstuuid= -overwrite_original -wm w -P "${_EXPORT_DIR}/${_imgfileout}"
       fi
     else
       _debug printf "%s Unsupported mime-type: %s" "${_imgfile}" "${_mime}"
